3bd113c5b50f849f91fda4aa760e1887c0346b92
leeyunhome <leeyunhome@daum.net>
2020-02-12 21:27:16 +0900

    PythonApplication1.py

---
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,16 @@
+
+student = {'name':'alghost', 'age':30, 'email' : 'alghost.lee@gmail.com'}
+print(student)
+print(student['name'])
+student['sex'] = 'male'
+print(student['sex'])
+
+
+sungsin07 = {}
+sungsin07['name'] = 'sungsin'
+print(sungsin07)
+
+keys = student.keys()
+print(keys)
+values = student.values()
+print(values)
